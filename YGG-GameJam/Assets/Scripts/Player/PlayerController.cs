160beb881bb617c4cf605635cc179fec89df2191
Randall Jay Ramirez <158044567+reigndeity@users.noreply.github.com>
2024-11-10 17:10:08 +0800

    Initial Player Controllers
    - Player gamepad controllers up to 4
    - Also added a keyboard input

---
+++ YGG-GameJam/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Properties")]
+    [SerializeField] private float speed = 5f; // Speed of the player movement
+    [SerializeField] private int playerID = 1; // Player ID (1, 2, 3, or 4)
+    [SerializeField] private bool isKeyboard;  // Toggle to use keyboard or controller
+
+    private void Update()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        if (isKeyboard)
+        {
+            // Keyboard Movement (WASD)
+            float horizontalKeyboard = Input.GetAxis("HorizontalKeyboard");
+            float verticalKeyboard = Input.GetAxis("VerticalKeyboard");
+            moveDirection = new Vector3(horizontalKeyboard, 0, verticalKeyboard).normalized;
+
+            // Keyboard Button Inputs (HJKL for AXBY)
+            if (Input.GetButtonDown("Keyboard_A")) // "H" key
+            {
+                Debug.Log("Keyboard A button (H key) pressed");
+            }
+
+            if (Input.GetButtonDown("Keyboard_X")) // "J" key
+            {
+                Debug.Log("Keyboard X button (J key) pressed");
+            }
+
+            if (Input.GetButtonDown("Keyboard_B")) // "K" key
+            {
+                Debug.Log("Keyboard B button (K key) pressed");
+            }
+
+            if (Input.GetButtonDown("Keyboard_Y")) // "L" key
+            {
+                Debug.Log("Keyboard Y button (L key) pressed");
+            }
+        }
+        else
+        {
+            // Joystick Movement (Controller)
+            float horizontalInput = Input.GetAxis("Horizontal" + playerID);
+            float verticalInput = Input.GetAxis("Vertical" + playerID);
+            moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+            // Controller Button Inputs
+            if (Input.GetButtonDown("Jump" + playerID)) // "A" button
+            {
+                Debug.Log("Player " + playerID + " A button (controller) pressed");
+            }
+
+            if (Input.GetButtonDown("Fire1_" + playerID)) // "X" button
+            {
+                Debug.Log("Player " + playerID + " X button (controller) pressed");
+            }
+
+            if (Input.GetButtonDown("Fire2_" + playerID)) // "B" button
+            {
+                Debug.Log("Player " + playerID + " B button (controller) pressed");
+            }
+
+            if (Input.GetButtonDown("Fire3_" + playerID)) // "Y" button
+            {
+                Debug.Log("Player " + playerID + " Y button (controller) pressed");
+            }
+        }
+
+        // Move the player based on the selected input method
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+    }
+}

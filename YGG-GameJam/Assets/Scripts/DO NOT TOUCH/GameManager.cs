4026a6c75ae8a14256d95c0b69a939e2e02a5068
Randall Jay Ramirez <158044567+reigndeity@users.noreply.github.com>
2024-11-16 07:57:09 +0800

    update 11/16 7:56 AM
    -- added temporary ui holders
    -- working game loop

--- YGG-GameJam/Assets/Scripts/DO NOT TOUCH/GameManager.cs
+++ YGG-GameJam/Assets/Scripts/DO NOT TOUCH/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -13,12 +14,25 @@ public class GameManager : MonoBehaviour
     public int gameModeType;
     public int recipeChosen;
     [Header("---------------------------------")]
+    public GameObject recipePanel;
+    public GameObject[] recipeImg;
+    [Header("---------------------------------")]
+    public float timeRemaining;
+    public TextMeshProUGUI timerTxt;
+    [Header("---------------------------------")]
+    [SerializeField] GameObject gameOverPanel;
+    [SerializeField] TextMeshProUGUI winnerTxt;
+
 
     [Header("Player Score Properties")]
     public int playerOneScore;
     public int playerTwoScore;
     public int playerThreeScore;
     public int playerFourScore;
+    [SerializeField] TextMeshProUGUI playerOneScoreTxt;
+    [SerializeField] TextMeshProUGUI playerTwoScoreTxt;
+    [SerializeField] TextMeshProUGUI playerThreeScoreTxt;
+    [SerializeField] TextMeshProUGUI playerFourScoreTxt;
 
     void Awake()
     {
@@ -36,14 +50,79 @@ public class GameManager : MonoBehaviour
         gameStart = false;
         StartCoroutine(GameBegins());
     }
+    void Update()
+    {
+        if (gameStart == true)
+        {
+            timeRemaining -= Time.deltaTime;
+            if (timeRemaining <= 0)
+            {
+                GameOver();
+                timeRemaining = 0;
+            }
+        }
+        timerTxt.text = timeRemaining.ToString("F0");
+        playerOneScoreTxt.text = "Team One: " + playerOneScore.ToString("F0");
+        playerTwoScoreTxt.text = "Team Two: " + playerTwoScore.ToString("F0");
+    }
 
     IEnumerator GameBegins()
     {
         recipeChosen = Random.Range (0,3);
+        recipePanel.SetActive(true);
+        switch(recipeChosen)
+        {
+            case 0:
+                recipeImg[0].SetActive(true);
+                break;
+            case 1:
+                recipeImg[1].SetActive(true);
+                break;
+            case 2:
+                recipeImg[2].SetActive(true);
+                break;
+        }
         yield return new WaitForSeconds(3);
+        recipePanel.SetActive(false);
         gameStart = true;
-
     }
 
+    public void CompareScores()
+    {
+        int highestScore = Mathf.Max(playerOneScore, playerTwoScore, playerThreeScore, playerFourScore);
+        if (highestScore == playerOneScore)
+        {
+            winnerTxt.text = "Team One Wins";
+        }
+        if (highestScore == playerTwoScore)
+        {
+            winnerTxt.text = "Team Two Wins";
+        }
+        if (highestScore == playerThreeScore)
+        {
+        }
+        if (highestScore == playerFourScore)
+        {
+        }
+
+        // Check if there is a tie between players with the highest score
+        int tieCount = 0;
+        if (playerOneScore == highestScore) tieCount++;
+        if (playerTwoScore == highestScore) tieCount++;
+        if (playerThreeScore == highestScore) tieCount++;
+        if (playerFourScore == highestScore) tieCount++;
+
+        if (tieCount > 1)
+        {
+            winnerTxt.text = "There is a Tie!";
+        }
+    }
+
+    public void GameOver()
+    {
+        gameStart = false;
+        CompareScores();
+        gameOverPanel.SetActive(true);
+    }
     
 }
